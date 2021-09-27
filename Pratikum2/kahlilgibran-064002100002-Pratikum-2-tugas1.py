56a3650ae4d4dbb25fe488f145f401c9dd5d935a
KahlilGibran13 <91047462+KahlilGibran13@users.noreply.github.com>
2021-09-27 14:22:49 +0700

    tugas 1 pratikum 2 algoritma

---
+++ Pratikum2/kahlilgibran-064002100002-Pratikum-2-tugas1.py
@@ -0,0 +1,43 @@
+# Pratikum-Algoritma
+Untuk Pratikum Algortima kelas bu ratna
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:42:02 2021
+
+@author: user
+"""
+
+"""
+
+nama : kahlil gibran
+nim 064002100002
+
+"""
+import math
+print("tugas praktikum 2 algoritma dasar")
+
+a = float(input("masukan nilai a = "))
+b = float(input("masukan nilai b = "))
+
+print("ini penjumlahan menggunakan math module : ",math.pow(a, b))
+
+hasil  = a + b
+print("jumlah a tambah b adalah",hasil)
+
+hasil = a - b
+print("jumlah a kurang b adalah",hasil)
+
+hasil = a * b
+print("jumlah a dikali b adalah",hasil)
+
+hasil = a / b
+print("jumlah a dibagi b adalah",hasil)
+
+hasil = a % b
+print("jumlah sisa pembagian a dan b adalah",hasil)
+
+hasil = math.log10(a)
+print("jumlah log a dan b adalah",hasil)
+
+hasil = a ** b
+print("jumlah a pangkat b adalah",hasil)
